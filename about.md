06ebbdfc91c67fbc158bb1c5e859310c044202fd
arthurliu <theonlyarthurliu@gmail.com>
2019-01-05 14:58:35 -0800

    About Me Update
    Still not published, working on image.

--- about.md
+++
@@ -1,9 +0,0 @@
----
-layout: default
-title: About
-permalink: /about/
-style: style3
-activeLink: about
----
-
-Hi, this page isn't finished yet.
--- scripts/smoothScroll.js
+++ scripts/smoothScroll.js
@@ -1,5 +1,6 @@
 function scrollToAnchor(anchor) {
 	// cgi_escape causes problems with multiple word tags which are combined with '+' symbols
+	// the following line resolves this by escaping + symbols
 	anchor = anchor.replace('+', '\\+');
     var scrollTo = $('#' + anchor);
     $('html,body').animate({scrollTop: scrollTo.offset().top},'slow');
@@ -8,7 +9,3 @@ function scrollToAnchor(anchor) {
 function scrollToTop(){
     $('html,body').animate({scrollTop: 0},'slow');
 }
-
-// function scrollToTest(){
-//     $('html,body').animate({scrollTop: $('#black\\+magic').offset().top},'slow');
-// }
