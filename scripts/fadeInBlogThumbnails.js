2b58df44eb1718f692c931f889240e5f62e6c403
arthurliu <theonlyarthurliu@gmail.com>
2019-01-10 13:41:14 -0800

    Javascript Update and About Update
    Removed javascript from pages that didn't use it. Also fixed about page height

--- scripts/fadeInBlogThumbnails.js
+++ scripts/fadeInBlogThumbnails.js
@@ -5,18 +5,18 @@ $(window).scroll(function(){
 	// 	console.log("hi");
 
 	if (wScroll > $('.blogShowcaseContainer').offset().top - ($(window).height() / 1.7)) {
-		console.log("hi");
-
 		$('.blogShowcaseContainer .blogThumbnail .blogName').each(function(i) {
-			console.log(i);
 			setTimeout(function(){
 				$('.blogThumbnail .blogName').eq(i).removeClass("hidden");
 			}, 150 * (i + 1));
 		});
 	} else {
-		var elem = $('.blogName');
-		if(!elem.hasClass('hidden')){
-		  elem.addClass('hidden');
-		}
+		// var elem = $('.blogName');
+		// if(!elem.hasClass('hidden')){
+		//   elem.addClass('hidden');
+		// }
+		$('.blogShowcaseContainer .blogThumbnail .blogName').each(function(i) {
+				$('.blogThumbnail .blogName').eq(i).addClass("hidden");
+		});
 	}
 });
